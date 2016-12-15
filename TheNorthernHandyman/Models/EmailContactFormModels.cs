bd2a7b40a50f1f1af11704da3fdb7bc4e404d2a1
skterbum <itismejody@gmail.com>
2016-12-15 10:21:27 -0600

    Added Contact Stuff

---
+++ TheNorthernHandyman/Models/EmailContactFormModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheNorthernHandyman.Models
+{
+    public class EmailContactFormModels
+    {
+        string toAddress { get; set; }
+        //string from
+        string message { get; set; }
+    }
+}
--- TheNorthernHandyman/Models/IdentityModels.cs
+++ TheNorthernHandyman/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace TheNorthernHandyman.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<TheNorthernHandyman.Models.ContactEmailModels> ContactEmailModels { get; set; }
     }
 }
